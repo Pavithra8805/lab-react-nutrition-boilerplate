62f44678140b525d388c32696af2f6faea61a01c
Pavithra8805 <pavithra8805@gmail.com>
2024-01-24 15:32:06 +0000

    First commit

---
+++ src/Component/Foodbox.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import './FoodBox.css';
+
+const Foodbox = (props) => {
+    const { data, index } = props;
+    const [amount, setAmount] = useState(0);
+    const [qty, setQty] = useState(0);
+
+    const handleAmount = (e) => {
+        if (e.target.value >= 0) {
+            setAmount(e.target.value);
+        } else {
+            setAmount(0);
+        }
+    };
+
+    const handleCheck = () => {
+        setQty(amount * data.cal);
+    };
+
+    const handleReset = () => {
+        setAmount(0);
+        setQty(0);
+    };
+
+
+    return (
+        <div className="main-container">
+            <div className="left-section">
+                <div className="image-container">
+                    <img src={data.img} style={{ width: "200px" }} />
+                </div>
+                <div className="food-detail">
+                    <p>{data.name}</p>
+                    <p>{data.cal}</p>
+                </div>
+                <div className="input-section">
+                    <input
+                        type="number"
+                        placeholder="Enter a numder"
+                        value={amount}
+                        onChange={handleAmount}
+                    />
+                    <button className="addBtn" onClick={handleCheck}>+</button>
+                </div>
+            </div>
+            <div className="right-section">
+                <p>
+                    {amount} {data.name} = {qty} Calories
+                </p>
+                <button className="reset-btn" onClick={handleReset}>Reset</button>
+            </div>
+        </div>
+    );
+};
+
+export default Foodbox;
